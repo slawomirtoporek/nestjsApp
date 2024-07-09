626de867e34c6a725018cc899796bace710c1f93
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-09 17:48:21 +0200

    Add LoggerInterceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggerInterceptor } from './shared/interceptors/logger.interceptor';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new LoggerInterceptor());
   await app.listen(3000);
 }
 bootstrap();
