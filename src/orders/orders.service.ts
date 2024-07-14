74971d68369d758b8e467e9a26fde538b28c927a
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-14 22:42:24 +0200

    Update the orders endpoints add a connect to the client table

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -8,14 +8,14 @@ export class OrdersService {
 
   public getAll(): Promise<Order[]> {
     return this.prismaService.order.findMany({
-      include: { product: true },
+      include: { product: true, client: true },
     });
   }
 
   public getById(id: Order['id']): Promise<Order | null> {
     return this.prismaService.order.findUnique({
       where: { id },
-      include: { product: true },
+      include: { product: true, client: true },
     });
   }
 
@@ -28,13 +28,16 @@ export class OrdersService {
   public async create(
     orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Order> {
-    const { productId, ...otherData } = orderData;
+    const { productId, clientId, ...otherData } = orderData;
     try {
       return await this.prismaService.order.create({
         data: {
           ...otherData,
           product: {
             connect: { id: productId },
+          },
+          client: {
+            connect: { id: clientId },
           },
         },
       });
@@ -49,13 +52,16 @@ export class OrdersService {
     id: Order['id'],
     orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Order> {
-    const { productId, ...otherData } = orderData;
+    const { productId, clientId, ...otherData } = orderData;
     return this.prismaService.order.update({
       where: { id },
       data: {
         ...otherData,
         product: {
           connect: { id: productId },
+        },
+        client: {
+          connect: { id: clientId },
         },
       },
     });
